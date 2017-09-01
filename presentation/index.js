d9d0e5e63036450b5dd19c874d577f276c6c8ca9
Jennifer Luker <jluker@deseretdigital.com>
2017-09-01 11:02:19 -0600

    Adding a lot of slides

--- presentation/index.js
+++ presentation/index.js
@@ -46,6 +46,9 @@ const images = {
   reactWeek: require("../assets/ReactWeek.png"),
   justAHeader: require("../assets/justAHeader.png"),
   importantRabbits: require("../assets/ImportantRabbits.png"),
+  beKind: require("../assets/beKind.png"),
+  livingStyleGuide: require("../assets/LivingStyleGuide.png"),
+  iconFont: require("../assets/IconFont.png"),
 };
 
 preloader(images);
@@ -53,10 +56,9 @@ preloader(images);
 const themeColors = {
   primary: "radial-gradient(#F5F5F5, #CECECE)",
   secondary: "#191919",
-  tertiary: "#03A9FC",
+  tertiary: "#981804",
   quartenary: "#CECECE",
 };
-
 
 const theme = createTheme(
   themeColors,
@@ -65,75 +67,140 @@ const theme = createTheme(
   }
 );
 
+const styles = {
+  li: {
+    margin: '70px 40px 0px 40px',
+    lineHeight: '2em'
+  },
+  liLargeText: {
+    fontSize: '1.5em',
+    fontWeight: 'bold'
+  }
+};
+
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["fade"]} transitionDuration={500} theme={theme}>
-        <Slide>
-          <Layout style={{transform: 'scale(1.5)'}}>
+      <Deck transition={["fade"]} transitionDuration={500} theme={theme} contentWidth="1768" contentHeight="932">
+        <Slide notes="Software engineer for over 10 years">
+          <Layout style={{padding: '0 200px'}}>
             <Fill>
-              <Image src={images.me} style={{width: '100%', borderRadius: '50%', }}/>
+              <Image margin="0" src={images.me} style={{maxWidth: '100%', borderRadius: '50%'}}/>
             </Fill>
             <Fill style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
-              <Heading textFont="secondary" size={3}>Jen Luker</Heading>
-              <Text style={{fontSize: '1.3rem'}}>Team Lead Software Engineer</Text>
-              <Text style={{fontSize: '1.3rem'}}>with Deseret Digital Media</Text>
+              <Heading size={1} textFont="secondary">Jen Luker</Heading>
+              <hr textFont="secondary" />
+              <Text style={{fontSize: '2rem', lineHeight: '2.4rem'}}>Team Lead Software Engineer</Text>
+              <Text style={{fontSize: '2rem', lineHeight: '2.4rem'}}>with Deseret Digital Media</Text>
+              <hr textFont="secondary" />
               <Link href="http://jenluker.com" >
-                <Text textColor="tertiary" style={{fontSize: '1.3rem'}}>http://jenluker.com</Text>
+                <Text textColor="tertiary" style={{fontSize: '2rem', lineHeight: '2.4rem'}}>http://jenluker.com</Text>
               </Link>
-              <Text style={{fontSize: '1.3rem'}}>Github: @knittingcodemonkey</Text>
-              <Text style={{fontSize: '1.3rem'}}>Twitter: @knitcodemonkey</Text>
+              <Text style={{fontSize: '2rem', lineHeight: '2.4rem'}}>Github: @knittingcodemonkey</Text>
+              <Text style={{fontSize: '2rem', lineHeight: '2.4rem'}}>Twitter: @knitcodemonkey</Text>
             </Fill>
           </Layout>
         </Slide>
 
         <Slide bgColor="primary">
-          <Layout style={{transform: 'scale(1.5)'}}>
+          <Layout>
             <Fill>
-              <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+              <Heading size={2} caps lineHeight={1} textColor="secondary">
                 Styling React for Reuse
               </Heading>
-              <Text margin="10px 0 0" textColor="tertiary" size={3} bold>
+              <Heading margin="10px 0 0" textColor="tertiary" size={3} bold>
                 an exploration in extensibility
-              </Text>
+              </Heading>
+              <br /><br />
+              <br /><br />
+              <Link src="">View slides @ http://styling-react-for-reuse.surge.sh</Link>
             </Fill>
           </Layout>
         </Slide>
 
-        <Slide textColor="primary" bgColor="secondary">
-          <Layout style={{transform: 'scale(1.5)'}}>
+        <Slide notes="Say Quote: 'We learn from failure much more than from success; we often discover what we will do by finding out what we will not do; and probably he who never made a mistake never made a discovery.'
+
+My department decided it was time to break up our one, very large classified product into several, smaller, vertical-based products such as cars, homes, jobs, services, and general classifieds. We realized that in order to maintain branding across all these new products, we would need to figure out a way of sharing UI elements. We started researching methods and technologies for creating independent components we could share.">
+          <Layout style={{margin: "0 90px"}} >
             <Fill>
-              <Text margin="10px 0 0" textColor="quartenary" fit style={{fontSize: '3.8rem'}}>
+              <Text fit style={{fontSize: '4.6rem'}}>
                 "We learn from failure much more than from success; we often discover what we will do by finding out what we will not do; and probably he who never made a mistake never made a discovery."</Text>
-              <Text textColor="quartenary" margin="50px 0 0" style={{fontSize: '1.8rem'}}>~ Samuel Smiles</Text>
+              <Text style={{fontSize: '3rem', marginTop: 100}}>~ Samuel Smiles</Text>
             </Fill>
           </Layout>
         </Slide>
 
-        <Slide bgColor="secondary">
-            <Layout style={{display: 'block', transform: 'scale(1.4)'}}>
+        <Slide
+          bgColor="secondary"
+          notes="This diagram is a visual representation of our codebase committed to Git around that time. It doesn’t include any additional code brought in through Composer. <br /><br />
+*click*<br /><br />
+
+Each dot is an individual file, and the little, tiny structure inside the yellow hexagram is our API. When I say we just wanted a little consistency; not another framework, This is why:<br /><br />
+
+*click*<br /><br />
+
+We were a PHP shop with Zend Framework, jQuery-ui development bundle, and an aging, custom-built, CMS. We had a whole lot of infrastructure to support an equally large amount of information; all of which was being funneled through a very small api. We figured good ole’ javascript and php would be good enough for us. <br /><br />
+
+We chose a designer, project manager, and a few developers, and started our pilot Front End Development Team with the sole purpose of building reusable components.">
+            <Layout style={{display: 'block', transform: 'scale(1.2)'}}>
               <Fill style={{width: '100%', height: '100%', position: 'relative', marginTop: -50}}>
-                <Text textColor="quartenary" style={{fontSize: '1.8rem', position: 'absolute', top: 0, left: 0}}>Committed to Github<br/>December 2015</Text>
-                <Image src={images.github1} style={{maxHeight: 580, width: 'auto', margin: '0 auto'}} />
+                <Text textColor="quartenary" style={{fontSize: '1.8rem', position: 'absolute', top: 60, left: 230}}>Committed to Github<br/>December 2015</Text>
+                <Image src={images.github1} style={{maxHeight: 580, width: 'auto', margin: '0.5em 0 0 -200px' }} />
                 <Appear>
-                  <Image src={images.github2} style={{position: "absolute", bottom: 18, left: 388, transform: 'scale(0.688)'}} />
+                  <Image src={images.github2} style={{position: "absolute", bottom: -73, right: 68, transform: 'scale(0.688)'}} />
                 </Appear>
                 <Appear>
-                  <Image src={images.github3} style={{position: "absolute", bottom: -99, left: -224, transform: 'scale(0.688)'}} />
+                  <Image src={images.github3} style={{position: "absolute", bottom: -191, right: 174, transform: 'scale(0.688)'}} />
                 </Appear>
-                <Text margin="60px 0 0" style={{fontSize: '2rem'}} textColor="quartenary">
-                  We just wanted a little consistency; not another framework.
-                </Text>
               </Fill>
+              <Text margin="60px 0 0" style={{fontSize: '2rem'}} textColor="quartenary">
+                We just wanted a little consistency; not another framework.
+              </Text>
             </Layout>
         </Slide>
 
-        <Slide bgImage={images.mhpNuke} />
+        <Slide bgImage={images.mhpNuke} notes="We named ourselves the MANHATTAN HARDCORE PROJECT (because we were going to make Atomic Components, hehe). Clearly we thought highly of ourselves. We had big ideas, and were going to make a big impact on the way we reused code. As soon as we started coding, the lessons started piling in. Clearly, failure was going to be our path to success."/>
+
+        <Slide style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', height: '100%'}}
+          notes="We built a shiny new living style guide that dynamically updates itself based on changes made to the git repo, and an SVG icon font. All the components were responsive, and the response was TERRIBLE.<br />
+Lessons we learned:
+<ul><li>Forcing all verticals to change their template to match a structure to support our fancy new modal, and menu.</li><li>Immediately changing that structure for other components.</li><li>Promising that our components would be drop-in replacements even if we had hard coded uniform data for the demo pages. (John and Jane Smith are great test names, right?)</li><li>Touting the beauty of changing the base component to change it everywhere. Unfortunately we were right on that one, and we suffered.</li></ul>">
+
+          <Layout style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+            <Fill>
+              <Image src={images.livingStyleGuide} />
+            </Fill>
+            <Fill>
+              <Image src={images.iconFont} />
+            </Fill>
+          </Layout>
+
+          <Layout style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+            <Fill>
+              <Heading size={4} style={{textAlign: 'center'}}>We are AWESOME!</Heading>
+              <ul style={{textAlign: 'left'}}>
+                <li>Living Style Guide</li>
+                <li>Half a dozen reusable components, including modal and menu</li>
+              </ul>
+            </Fill>
+            <Fill>
+              <Heading size={4} style={{textAlign: 'center'}}>So, that was bad</Heading>
+              <ul style={{textAlign: 'left'}}>
+                <li>Forcing all verticals to change their template to match a structure to support our fancy new modal, and menu.</li>
+                <li>Immediately changing that structure for other components.</li>
+                <li>Promising that our components would be drop-in replacements even if we had hard coded uniform data for the demo pages. (John and Jane Smith are great test names, right?)</li>
+                <li>Touting the beauty of changing the base component to change it everywhere. Unfortunately we were right on that one.</li>
+                <li>SVG Icon Font</li>
+              </ul>
+            </Fill>
+          </Layout>
+        </Slide>
+
 
         <Slide>
-          <Layout style={{display: 'block', transform: 'scale(1.5)'}}>
+          <Layout style={{display: 'block'}}>
             <Fill style={{width: '100%', height: '100%', position: 'relative'}}>
-              <Image src={images.reactWeek} style={{maxWidth: 920, maxHeight: 620, width: 'auto', margin: '0 auto'}} />
+              <Image src={images.reactWeek} style={{maxWidth: 1768, maxHeight: 780, width: 'auto', margin: '0 auto'}} />
             </Fill>
             <Fill>
               <Heading textColor="secondary" textFont="primary" size={4} style={{fontWeight: 'normal'}}>React Week</Heading>
@@ -141,28 +208,78 @@ export default class Presentation extends React.Component {
           </Layout>
         </Slide>
 
-        <Slide>
-          <Layout style={{display: 'block', transform: 'scale(1.5)'}}>
-            <Fill style={{width: '100%', height: '100%', position: 'relative'}}>
-              <Image src={images.justAHeader} style={{maxWidth: 920, maxHeight: 620, width: 'auto', margin: '0 auto'}} />
+        <Slide bgColor="primary" notes="It turns out our header included dropdown menus, responsive hamburger menus, modals, dynamic login detection, dynamic menu resizing on scroll, weather modules with geolocation, icon fonts, and a search bar. It was huge, required jQuery, and came with a whole host of webpack/bower/gulp build process complexity that none of us knew anything about. As we coded, our designers, and the technology, continued to innovate. We were learning (and refactoring) a lot, but the old Manhattan Hardcore Components were still in circulation, and as we completed the new header components, those were being incorporated into various websites, as well.">
+          <Layout>
+            <Fill>
+          <CodePane
+            lang="js"
+            source={require("raw-loader!../assets/code/header.example")}
+            margin="0px auto"
+            textSize="0.6em"
+          />
             </Fill>
             <Fill>
-              <Heading textColor="secondary" textFont="primary" size={4} style={{fontWeight: 'normal'}}>"Just a header," we said</Heading>
+              <CodePane
+                lang="js"
+                source={require("raw-loader!../assets/code/header2.example")}
+                margin="0px auto"
+                textSize="0.6em"
+              />
+            </Fill>
+          </Layout>
+          <Heading margin="60px auto 0px auto" textColor="secondary" textFont="primary" size={4} style={{fontWeight: 'normal'}}>"Just the header," we said</Heading>
+        </Slide>
+
+        <Slide>
+          <Layout style={{display: 'block'}}>
+            <Fill style={{width: '100%', height: '100%', position: 'relative'}}>
+              <Image src={images.importantRabbits} style={{maxWidth: 1768, maxHeight: 852, width: 'auto', margin: '0 auto'}} />
             </Fill>
           </Layout>
         </Slide>
 
         <Slide>
-          <Layout style={{display: 'block', transform: 'scale(1.5)'}}>
+          <Text>the</Text>
+          <Heading size={1} textColor="tertiary" caps>one true answer</Heading>
+          <Text>to all our problems is...</Text>
+        </Slide>
+        <Slide>
+          <Layout style={{display: 'block'}}>
             <Fill style={{width: '100%', height: '100%', position: 'relative'}}>
-              <Image src={images.importantRabbits} style={{maxWidth: 920, maxHeight: 620, width: 'auto', margin: '0 auto'}} />
+              <Image src={images.beKind} style={{maxWidth: 1768, maxHeight: 852, width: 'auto', margin: '0 auto'}} />
+            </Fill>
+          </Layout>
+        </Slide>
+
+        <Slide textColor="secondary">
+          <Layout>
+            <Fill style={{textAlign: 'left'}}>
+              <Heading fit caps style={{textAlign: 'center'}}>What do I consider When choosing a component?</Heading>
+              <hr />
+              <ul>
+                <li style={styles.li}>
+                  <Text style={styles.liLargeText}>Use traditional DOM elements as they were intended</Text>
+                  <Text style={{fontSize: '1.0em'}}>If you need a button, use a button. Accessibility users will love you for it.</Text>
+                </li>
+                <li style={styles.li}>
+                  <Text style={styles.liLargeText}>Override styles within third-party component with your own styles</Text>
+                  <Text style={{fontSize: '1.0em'}}>Transitions, and animations are easy to embed, but difficult to overwrite.</Text>
+                </li>
+                <li style={styles.li}>
+                  <Text style={styles.liLargeText}>Rearrange component’s visual layout</Text>
+                  <Text style={{fontSize: '1.0em'}}>What if the design places the buttons lined up along the right side instead of the bottom?</Text>
+                </li>
+                <li style={styles.li}>
+                  <Text style={styles.liLargeText}>Use external assets for icons, logos, and images</Text>
+                  <Text style={{fontSize: '1.0em'}}>This is not just for design, but also for globalization. Imagery differs between cultures.</Text>
+                </li>
+              </ul>
             </Fill>
           </Layout>
         </Slide>
 
         <CodeSlide
           bgColor="secondary"
-          style={{marginTop: -150}}
           transition={[]}
           lang="js"
           code={require("raw-loader!../assets/code/blah.example")}
